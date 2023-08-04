d5aeefac4469026cea5e49d03484a317b7b4e34c
ravi barla <ravibarla1@gmail.com>
2023-08-04 16:38:29 +0530

    nothing new commited

--- src/components/ToDoForm/ToDoForm.js
+++ src/components/ToDoForm/ToDoForm.js
@@ -1,29 +1,31 @@
 import { useState } from "react";
-import "./ToDoForm.css";
 import { useDispatch } from "react-redux";
-import { addTodo } from "../../redux/actions/todoActions";
+import {addTodo} from "../../redux/actions/todoActions";
+
+import "./ToDoForm.css";
+
 function ToDoForm() {
   const [todoText, setTodoText] = useState("");
-  const dispatch = useDispatch();
+  const disptach = useDispatch();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addTodo(todoText));
     setTodoText("");
+    disptach(addTodo(todoText));
   };
 
   return (
     <div className="container">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="form-control mb-3"
-          value={todoText}
-          onChange={(e) => setTodoText(e.target.value)}
-        />
-        <button className="btn btn-success float-end" type="submit">
-          Create Todo
-        </button>
-      </form>
+      
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="form-control mb-3"
+        value={todoText}
+        onChange={(e) => setTodoText(e.target.value)}
+      />
+      <button className="btn btn-success float-end" type="submit">Create Todo</button>
+    </form>
     </div>
   );
 }
