a1d7aa1a81a980d991bd2b5f48edd2be5bae3800
ravi barla <ravibarla1@gmail.com>
2023-08-04 10:38:40 +0530

    dispatch the addTodo action

--- src/components/ToDoForm/ToDoForm.js
+++ src/components/ToDoForm/ToDoForm.js
@@ -1,27 +1,30 @@
 import { useState } from "react";
 import "./ToDoForm.css";
-
-function ToDoForm({ onCreateTodo }) {
+import { useDispatch } from "react-redux";
+import { addTodo } from "../../redux/actions/todoActions";
+function ToDoForm() {
   const [todoText, setTodoText] = useState("");
-
+  const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
-    onCreateTodo(todoText);
+
+    dispatch(addTodo);
     setTodoText("");
   };
 
   return (
     <div className="container">
-      
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="form-control mb-3"
-        value={todoText}
-        onChange={(e) => setTodoText(e.target.value)}
-      />
-      <button className="btn btn-success float-end" type="submit">Create Todo</button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-control mb-3"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+        />
+        <button className="btn btn-success float-end" type="submit">
+          Create Todo
+        </button>
+      </form>
     </div>
   );
 }
