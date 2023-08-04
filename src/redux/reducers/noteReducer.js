0ff07e09ddaf543a029f129fc6126d99f2f47870
ravi barla <ravibarla1@gmail.com>
2023-08-04 15:56:56 +0530

    adding store,reducer,action,combined reducer for note app

---
+++ src/redux/reducers/noteReducer.js
@@ -0,0 +1,23 @@
+import { ADD_NOTE, DELETE_NOTE } from "../../redux/actions/noteAction";
+const initialState = {
+  notes: [
+    {
+      text: "Note 1",
+      createdOn: new Date(),
+    },
+  ],
+};
+export default function noteReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_NOTE:
+      return {
+        ...state,
+        notes: [...state.notes, { text: action.text, createdOn: new Date() }],
+      };
+    case DELETE_NOTE:
+      state.notes.splice(action.index, 1);
+      return {};
+    default:
+      return state;
+  }
+}
--- src/store.js
+++ src/store.js
@@ -1,7 +1,13 @@
 import { createStore } from "redux";
 // import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./redux/reducers/todoReducer";
-
-const store = createStore(todoReducer);
+import { combineReducers } from "redux";
+import noteReducer from "./redux/reducers/noteReducer";
+const result=combineReducers({
+    notes:noteReducer
+    todos:todoReducer
+})
+// const store = createStore(todoReducer);
 // const store = configureStore({ reducer: todoReducer });
+const store = createStore(result);
 export default store;
