736e843ebe848c1ed908be33f15ce01c8d8448f5
ravi barla <ravibarla1@gmail.com>
2023-08-05 12:17:45 +0530

    adding todo slice

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,4 +1,5 @@
-import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
+// import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
+const { createSlice } = require("@reduxjs/toolkit");
 const initialState = {
   todos: [
     {
@@ -11,24 +12,48 @@ const initialState = {
     },
   ],
 };
-export default function todoReducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return {
-        ...state,
-        todos: [...state.todos, { text: action.text, completed: false }],
-      };
-    case TOGGLE_TODO:
-      return {
-        ...state,
-        todos: state.todos.map((todo, i) => {
-          if (i === action.index) {
-            todo.completed = !todo.completed;
-          }
-          return todo;
-        }),
-      };
-    default:
-      return state;
-  }
-}
+//create reducer using redux/toolkit
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    //this is add action
+    add: (state, action) => {
+      state.todos.push({
+        text: action.payload,
+        completed: false,
+      });
+    },
+    toggle: (state, action) => {
+      state.todos.map((todo, i) => {
+        if (i === action.payload) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+    },
+  },
+});
+//reducer using redux
+
+// export default function todoReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case ADD_TODO:
+//       return {
+//         ...state,
+//         todos: [...state.todos, { text: action.text, completed: false }],
+//       };
+//     case TOGGLE_TODO:
+//       return {
+//         ...state,
+//         todos: state.todos.map((todo, i) => {
+//           if (i === action.index) {
+//             todo.completed = !todo.completed;
+//           }
+//           return todo;
+//         }),
+//       };
+//     default:
+//       return state;
+//   }
+// }
